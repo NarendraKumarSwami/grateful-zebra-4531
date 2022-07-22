457026c80bc6d77696d7cc8f1a45957c97094489
NarendraKumarswami <narendrakumarswami786@gmail.com>
2022-07-22 17:02:29 +0530

    mobile otp verification page is added

--- components/language_options.js
+++ components/language_options.js
@@ -1,12 +1,26 @@
 // lagouguage options 
+ 
+let addLanguage = (container) =>{
+        container.inerText = '';
+        let  select = document.createElement('select');
+        let option1 = document.createElement('option');
+        option1.setAttribute('value', 'English');
+        option1.innerText = 'English';
+        let option2 = document.createElement('option');
+        option2.setAttribute('value', 'हिन्दी');
+        option2.innerText = 'हिन्दी';
+        let option3 = document.createElement('option');
+        option3.setAttribute('value', 'తెలుగు');
+        option3.innerText = 'తెలుగు';
+        let option4 = document.createElement('option');
+        option4.setAttribute('value', 'தமிழ்');
+        option4.innerText  = 'தமிழ்';
 
-document.getElementById('language').addEventListener('change', ()=>{
-               let url = ``
-})
+        select.append(option1, option2, option3, option4);
 
-document.querySelector('#language_select').addEventListener('load', myHead);
+        container.append(select);
 
-
-let myHead = ()=>{
-        
 }
+
+
+export { addLanguage}
--- scripts/crowdfunding.js
+++ scripts/crowdfunding.js
@@ -1,16 +1,21 @@
 // crowdfuncding 
+import { addLanguage} from '../components/language_options.js';
 
+   window.onload = ()=>{
+          addLanguage(document.getElementById('language'));
+          console.log('Function called')
+}
 
 document.querySelector('form').addEventListener('submit', () =>{
       event.preventDefault();
       let data = event.target.children;
      let user = new User(data[0].children[1].value, data[1].value, data[2].value, data[3].value, data[4].value)
-     let userData = JSON.parse(localStorage.getItem('users'))  || [];
+     let userData =  [];
      userData.push(user);
      localStorage.setItem('users', JSON.stringify(userData));
 
      // location to new page 
-     
+      window.location = './mobileOtp.html';
 })
 
 // object fucntion for sign up
