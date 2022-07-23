5baf7be8bfc894f50c8e685ef62d97fb1d771895
NarendraKumarswami <narendrakumarswami786@gmail.com>
2022-07-23 07:40:33 +0530

    sign_in.js is aaded

--- scripts/crowdfunding.js
+++ scripts/crowdfunding.js
@@ -1,3 +1,18 @@
+// import navbar.js for navbar 
+  import {navbar} from '../components/navbar.js';
+// import sign_in.js file for append
+  import {appendPopLogin} from '../components/sign_in.js';
+
+window.onload  = () =>{
+          let link = document.createElement('link');
+          link.rel = 'stylesheet';
+          link.href = './styles/nav.css';
+          // append this link to head of document
+          document.querySelector('head').append(link);
+          document.getElementById('navbar').innerHTML = navbar();
+
+       appendPopLogin();
+}
 // crowdfuncding 
 
 
