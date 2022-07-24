7dc89b74c72cda5be707f2e79279705e8c3d0f50
AbhishekAryanWeb170901 <bittoofoswami@gmail.com>
2022-07-24 20:30:17 +0530

    works on-how it works, login css, crowd funding, want to start css

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -63,17 +63,17 @@ productContainers.forEach((item, i) => {
   function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+    let dotss = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < dotss.length; i++) {
+      dotss[i].className = dotss[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    dotss[slideIndex-1].className += " active";
   }
 // slide 2 end
 
